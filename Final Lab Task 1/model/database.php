a61b7bae2dbc0cca24407bb888998df5e4ffa50f
Eshrat Ara Mumu <ishratmumu607@gmail.com>
2026-05-14 04:08:37 +0600

    Database configuration

---
+++ Final Lab Task 1/model/database.php
@@ -0,0 +1,14 @@
+<?php
+    $dns = 'mysql:host = localhost; dbname = travel_guide_db';
+    $username = 'root';
+
+
+    try {
+        $db = new PDO($dns, $username);
+    } catch (PDOException $e) {
+        $error = "Database Error: ";
+        $error .= $e->getMessage();
+        include('view/error.php');
+        exit();
+    }
+?>
